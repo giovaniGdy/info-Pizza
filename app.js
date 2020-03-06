a2d74f450eba61b2aabdd0613d751426f1f7a865
giovaniGdy <giovani.godoy@sou.unifeob.edu.br>
2020-03-06 13:54:59 -0300

    Remoção de comentários desnecessários

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const expressValidator = require("express-validator")
 const methodOverride = require("method-override")
 var img = require('path').join(__dirname,'./public/')
 
-// aaaa
 var cors = require('cors')
-
 app.use(cors({origin: '*'}));
-// aaaa
 
 app.use(express.static( img ) )
 app.use(methodOverride("_method"))
